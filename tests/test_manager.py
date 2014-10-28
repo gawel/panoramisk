08f52a58298da7090b6e33dafd11c4d7f52d00ee
Gael Pasgrimaud <gael@gawel.org>
2014-10-28 12:10:50 +0100

    use more pytest

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,64 +1,67 @@
 # -*- coding: utf-8 -*-
-from unittest import TestCase
 import os
+import pytest
 from panoramisk import testing
 from panoramisk import message
 from panoramisk.utils import asyncio
 
+test_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-class TestManager(TestCase):
 
-    test_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
-
-    def callFTU(self, stream=None, **config):
+@pytest.fixture
+def manager():
+    def manager(stream=None, **config):
         if stream:
-            config['stream'] = os.path.join(self.test_dir, stream)
-        manager = testing.Manager(**config)
-        return manager
+            config['stream'] = os.path.join(test_dir, stream)
+        return testing.Manager(**config)
+    return manager
 
-    def test_connection(self):
-        manager = self.callFTU(loop=None)
-        self.assertTrue(manager.connect())
-        self.assertTrue('bla')
 
-    def test_ping(self):
-        manager = self.callFTU(stream='ping.yaml')
-        future = manager.send_action({'Action': 'Ping'})
-        self.assertIn('ping', future.result().lheaders)
+def test_connection(manager):
+    assert isinstance(manager().connect(), asyncio.Task)
 
-    def test_queue_status(self):
-        manager = self.callFTU(stream='queue_status.yaml')
-        future = manager.send_action({'Action': 'QueueStatus',
-                                      'Queue': 'xxxxxxxxxxxxxxxx-tous'})
-        responses = future.result()
-        self.assertEqual(len(responses), 9)
 
-    def test_get_variable(self):
-        manager = self.callFTU(stream='get_variable.yaml')
-        future = manager.send_action({'Command': 'GET VARIABLE "PEERNAME"'})
-        responses = future.result()
-        self.assertEqual(len(responses), 2)
+def test_ping(manager):
+    manager = manager(stream='ping.yaml')
+    future = manager.send_action({'Action': 'Ping'})
+    assert 'ping' in future.result().lheaders
 
-    def test_close(self):
-        manager = self.callFTU()
-        manager.close()
 
-    def test_events(self):
-        future = asyncio.Future()
+def test_queue_status(manager):
+    manager = manager(stream='queue_status.yaml')
+    future = manager.send_action({'Action': 'QueueStatus',
+                                  'Queue': 'xxxxxxxxxxxxxxxx-tous'})
+    responses = future.result()
+    assert len(responses) == 9
 
-        def callback(event, manager):
-            future.set_result(event)
 
-        manager = self.callFTU(use_http=True, url='http://host')
-        manager.register_event('Peer*', callback)
-        event = message.Message.from_line('Event: PeerStatus',
-                                          manager.callbacks)
-        self.assertTrue(event.success)
-        self.assertEqual(event['Event'], 'PeerStatus')
-        self.assertIn('Event', event)
-        manager.dispatch(event, event.matches)
-        self.assertTrue(event is future.result())
+def test_get_variable(manager):
+    manager = manager(stream='get_variable.yaml')
+    future = manager.send_action({'Command': 'GET VARIABLE "PEERNAME"'})
+    responses = future.result()
+    assert len(responses) == 2
 
-        event = message.Message.from_line('Event: NoPeerStatus',
-                                          manager.callbacks)
-        self.assertTrue(event is None)
+
+def test_close(manager):
+    manager().close()
+
+
+def test_events(manager):
+    future = asyncio.Future()
+
+    def callback(event, manager):
+        future.set_result(event)
+
+    manager = manager()
+    manager.register_event('Peer*', callback)
+    event = message.Message.from_line('Event: PeerStatus',
+                                      manager.callbacks)
+    assert event.success is True
+    assert event['Event'] == 'PeerStatus'
+    assert 'Event' in event
+    manager.dispatch(event, event.matches)
+    assert event is future.result()
+
+    event = message.Message.from_line('Event: NoPeerStatus',
+                                      manager.callbacks)
+    assert event is None
