64f7565fa5754c85551502d0e96dad51415c5d1b
Gael Pasgrimaud <gael@gawel.org>
2016-10-14 11:17:05 +0200

    allow pytest to scan examples/

--- examples/fast_agi_server.py
+++ examples/fast_agi_server.py
@@ -13,19 +13,23 @@ def call_waiting(request):
     pprint((yield from request.send_command('EXEC Wait 30')))
 
 
-fa_app = fast_agi.Application(loop=loop)
-fa_app.add_route('call_waiting', call_waiting)
-coro = asyncio.start_server(fa_app.handler, '0.0.0.0', 4574, loop=loop)
-server = loop.run_until_complete(coro)
+def main():
+    fa_app = fast_agi.Application(loop=loop)
+    fa_app.add_route('call_waiting', call_waiting)
+    coro = asyncio.start_server(fa_app.handler, '0.0.0.0', 4574, loop=loop)
+    server = loop.run_until_complete(coro)
 
-# Serve requests until CTRL+c is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    # Serve requests until CTRL+c is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+if __name__ == '__main__':
+    main()
